35db24bd6cc7f2441403c3330ec4193fcd1b6cbd
varun <varund520@gmail.com>
2021-11-30 13:22:15 -0400

    rollback all things

--- firstapp/src/components/SubComponent/ExpenseItem.js
+++ firstapp/src/components/SubComponent/ExpenseItem.js
@@ -7,13 +7,9 @@ function ExpenseItem(props){
             <div className="expense-item__description">
                 <h2>{ props.title }</h2>
                 <div className="expense-item__price">${ props.price }</div>
-
-                <div>this is the test</div>
-
             </div>
         </div>
     );
 }
 
 export default ExpenseItem;
-
